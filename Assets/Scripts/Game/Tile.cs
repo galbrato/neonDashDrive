15998ce755be6263855b2e5b62dff8ff979066c3
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-08-06 17:17:19 -0300

    movimentação por tile implementada, com curva de movimentação

---
+++ Assets/Scripts/Game/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Tile upTile;
+    public Tile rightTile;
+    public Tile downTile;
+    public Tile leftTile;
+
+    public bool isOccupied;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
