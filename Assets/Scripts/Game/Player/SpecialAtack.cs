84c9bb335f6f219c5254633c90c8408d52ddd144
Gabriel Simmel Nascimento <galbrato@gmail.com>
2019-08-08 13:15:16 -0300

    Criei a classe abstrata de atack especial

---
+++ Assets/Scripts/Game/Player/SpecialAtack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SpacialAtack : MonoBehaviour {
+    abstract public bool StartAtack();
+
+    abstract public bool CanAtack();
+
+    abstract public bool IsAtacking();
+
+    abstract public bool EndAtack();
+}
