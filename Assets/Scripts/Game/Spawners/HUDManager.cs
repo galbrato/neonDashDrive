3cac6a986fca215a6c255a74e3c94ac33c4a0ab9
Fabrício <fabricio-gf@hotmail.com>
2019-08-09 13:02:03 -0300

    Added fonts and more assets

--- Assets/Scripts/Game/Spawners/HUDManager.cs
+++ Assets/Scripts/Game/Spawners/HUDManager.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HUDManager : MonoBehaviour
 {
-    //lifecount
-    //bombcount
-    //points
+    [SerializeField] int lifeCount = 3;
+    [SerializeField] int bombCount = 1;
+    [SerializeField] int points = 0;
     //playersprite
     //playername
+
+    [SerializeField] GameObject[] lifeIcons;
+    [SerializeField] GameObject[] bombIcons;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     public void InitializeHUD()
     {
 
+    }
+
+    public void UpdateLives(int change)
+    {
+        lifeCount += 3;
+        if (lifeCount > 3) lifeCount = 3;
+        else if (lifeCount < 0) lifeCount = 0;
+
+
+    }
+
+    public void UpdateText(int change)
+    {
+        points += change;
+        scoreText.text = points.ToString();
     }
 }
