4c5d65ac870619fae583e9ec1d2ac41c8e920b8c
Fabricio <fabricio-gf@hotmail.com>
2019-08-09 22:18:41 -0300

    Added pause script and created animator

---
+++ Assets/Scripts/Game/PauseManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    bool isPaused = false;
+
+    [SerializeField] Animator pauseAnimator;
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        if (isPaused)
+        {
+            pauseAnimator.SetTrigger("PauseOn");
+        }
+        else
+        {
+            pauseAnimator.SetTrigger("PauseOff");
+        }
+    }
+}
