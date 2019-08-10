5b13a073def03e7836639b2a05ab14292d6e64b5
Fabricio <fabricio-gf@hotmail.com>
2019-08-09 22:13:35 -0300

    Added powerups basic behaviour

---
+++ Assets/Scripts/Game/Player/PlayerPowerupPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPowerupPickup : MonoBehaviour
+{
+    public delegate void PlayerPowerupDelegate();
+    public PlayerPowerupDelegate OnShotPickup;
+    public PlayerPowerupDelegate OnShieldPickup;
+    public PlayerPowerupDelegate OnBombPickup;
+    public PlayerPowerupDelegate OnLifePickup;
+
+    public void IncrementShotLevel()
+    {
+        
+        OnShotPickup?.Invoke();
+    }
+
+    public void AddShield()
+    {
+        OnShieldPickup?.Invoke();
+    }
+
+    public void AddBomb()
+    {
+        OnBombPickup?.Invoke();
+    }
+
+    public void AddLife()
+    {
+        OnLifePickup?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Game/Powerups/Powerup.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    float angularVelocity;
+    public float maxVelocity;
+    public float minVelocity;
+
+    public float moveSpeed;
+
+    public PowerupType myType;
+
+    public enum PowerupType
+    {
+        SHOT,
+        SHIELD,
+        BOMB,
+        LIFE
+    }
+
+    Transform child;    
+
+    public void Awake()
+    {
+        angularVelocity = Random.Range(minVelocity, maxVelocity);
+        if(Random.value >= 0.5f)
+        {
+            angularVelocity = -angularVelocity;
+        }
+        child = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        child.Rotate(new Vector3(0, 0, 1), angularVelocity);
+        transform.Translate(Vector3.down*moveSpeed*Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print(collision.tag);
+        if (collision.CompareTag("Player"))
+        {
+            PlayerPowerupPickup pl = collision.GetComponent<PlayerPowerupPickup>();
+            switch (myType)
+            {
+                case PowerupType.SHOT:
+                    pl.IncrementShotLevel();
+                break;
+                case PowerupType.SHIELD:
+                    pl.AddShield();
+                break;
+                case PowerupType.BOMB:
+                    pl.AddBomb();
+                break;
+                case PowerupType.LIFE:
+                    pl.AddLife();
+                break;
+            }
+            Destroy(gameObject);
+        }
+    }
+}
