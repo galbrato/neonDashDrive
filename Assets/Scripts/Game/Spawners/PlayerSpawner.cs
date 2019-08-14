933e8c44e285db3ddc72b55db26e83ce1a5e94b9
Fabricio <fabricio-gf@hotmail.com>
2019-08-14 14:00:02 -0300

    Added initial tile to player spawn

--- Assets/Scripts/Game/Spawners/PlayerSpawner.cs
+++ Assets/Scripts/Game/Spawners/PlayerSpawner.cs
@@ -6,10 +6,14 @@ public class PlayerSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] playerPrefab = null;
     [SerializeField] Vector3 spawnPosition = new Vector3(0,0,0);
+    [SerializeField] Transform tilesGrid = null;
+    [SerializeField] int spawnTileIndex;
     [SerializeField] Transform spawnParent = null;
 
     public GameObject SpawnPlayer(int index)
     {
-        return Instantiate(playerPrefab[index], spawnPosition, Quaternion.identity, spawnParent);
+        GameObject obj = Instantiate(playerPrefab[index], spawnPosition, Quaternion.identity, spawnParent);
+        obj.GetComponent<TileMovement>().ActualTile = tilesGrid.GetChild(spawnTileIndex).GetComponent<Tile>();
+        return obj;
     }
 }
